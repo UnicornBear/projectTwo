4c251520c9cac3d180a74d1956a83be815b71117
UnicornBear <jtbregenzer@gmail.com>
2019-06-25 19:25:35 -0600

    starter files for project two

--- models/index.js
+++ models/index.js
@@ -1,30 +1,32 @@
-// utilized activity code for index 
-'use strict';
+"use strict";
 
-// dependencies (require imports)
-var fs = require('fs');
-var path = require('path');
-var Sequelize = require('sequelize');
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || 'development';
-var config = require(__dirname + '/../config/config.json')[env];
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
-// sequelized
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
   .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
+    var model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
@@ -37,5 +39,4 @@ Object.keys(db).forEach(function(modelName) {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-// export module to db
 module.exports = db;
